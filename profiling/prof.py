d4f0ee70ecd926b83124bbd0afa99086c93023c2
basiav <82549088+basiav@users.noreply.github.com>
2024-07-02 13:22:52 +0200

    Profiling update

--- profiling/prof.py
+++ profiling/prof.py
@@ -4,7 +4,6 @@ import networkx as nx
 import os
 
 from Qommunity.samplers.hierarchical.advantage_sampler import AdvantageSampler
-# from Qommunity.samplers.hierarchical.gurobi_sampler import GurobiSampler
 from Qommunity.searchers.hierarchical_community_searcher import HierarchicalCommunitySearcher
 
 
@@ -38,41 +37,41 @@ except:
 
 
 
-dir = "profiling"
+dir = "output"
 os.makedirs(os.path.dirname(dir+"/"), exist_ok=True)
 make_dir(f"{dir}/{solver}")
 
 
 N_RUNS = 1
 
-G_idx = MAX_NODES//MIN_NODES - 1
+big_graph_idx = MAX_NODES//MIN_NODES - 1
 
-mods = np.zeros((num_nodes[:G_idx].shape[0], N_RUNS))
-comms = np.empty((num_nodes[:G_idx].shape[0], N_RUNS), dtype=object)
+
+mods = np.zeros((num_nodes[big_graph_idx:].shape[0], N_RUNS))
+comms = np.empty((num_nodes[big_graph_idx:].shape[0], N_RUNS), dtype=object)
 
 mods_graph_N = np.zeros((N_RUNS))
 comms_graph_N = np.empty((N_RUNS), dtype=object)
-for i, G in tqdm(enumerate(Graphs[G_idx:])):
-    current_net_size = int(G.number_of_nodes())    
+for i, G in tqdm(enumerate(Graphs[big_graph_idx:])):
+    advantage = AdvantageSampler(G, num_reads=100)
+    hierch_searcher = HierarchicalCommunitySearcher(advantage)
+    
+    # current_net_size = int(num_nodes[i])
+    current_net_size = int(G.number_of_nodes())
     for r in range(N_RUNS):
-        # new instance of the sampler each time
-        advantage = AdvantageSampler(G, num_reads=100)
-        hierch_searcher = HierarchicalCommunitySearcher(advantage)
         comms_res = hierch_searcher.hierarchical_community_search()
-        
         mod_score = nx.community.modularity(G, comms_res)
         mods_graph_N[r] = mod_score
         comms_graph_N[r] = comms_res
 
+        # For bigger graphs computations take time, so better save it up
+        # even for each run
         try:
             np.save(f"{dir}/{solver}/{solver}-network_size_{current_net_size}", mods_graph_N)
             np.save(f"{dir}/{solver}/{solver}-network_size_{current_net_size}_comms", comms_graph_N)
         except Exception as e:
-            print(f"iter: {i} run: {r} saving failed")
-            print(e)
+            print(f"iter {i} run {r} npy saving failed:\n{e}")
 
-    # current_net_size = int(num_nodes[i])
-    current_net_size = int(G.number_of_nodes())
     mods[i] = mods_graph_N
     comms[i] = comms_graph_N
     np.save(f"{dir}/{solver}/{solver}-network_size_{current_net_size}", mods_graph_N)
